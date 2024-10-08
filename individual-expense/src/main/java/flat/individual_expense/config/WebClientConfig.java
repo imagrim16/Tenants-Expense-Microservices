77e6a2616e73579efd9cd29f2be8e538a1ff0a7d
imagrim16 <imagrim16@gmail.com>
2024-10-08 23:10:46 +0530

    Updating the localhost to the generic names of the services

--- individual-expense/src/main/java/flat/individual_expense/config/WebClientConfig.java
+++ individual-expense/src/main/java/flat/individual_expense/config/WebClientConfig.java
@@ -1,8 +1,10 @@
 package flat.individual_expense.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Configuration
 public class WebClientConfig {
